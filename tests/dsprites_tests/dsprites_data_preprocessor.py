da2146af50aff6a6998910296af112f99354f9fa
Sonja Krafft <ga83bin@mytum.de>
2024-03-14 13:34:54 +0100

    add test data (Pytorch model for ECG data and dsprites dataset) and clean code

--- tests/dsprites_tests/dsprites_data_preprocessor.py
+++ tests/dsprites_tests/dsprites_data_preprocessor.py
@@ -17,6 +17,7 @@ class DSprites(DataPreprocessor):
         self.discrete_features = self.get_discrete_features()
 
     def get_meta_and_z_data(self, model_num=0):
+        # get file from trained model of Locatello et al.
         df = pd.read_csv("./data/dsprites" + str(model_num) + ".csv").head(self.size)
 
         z = np.squeeze((df['mean'].map(lambda x: np.matrix(x)).tolist()))
